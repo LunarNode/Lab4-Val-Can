efbde7e6be72460a1aa546b2f44b545bff764df4
LunarNode <veeramanival@gmail.com>
2021-12-14 17:29:24 -0800

    added pypi files

---
+++ setup.py
@@ -0,0 +1,6 @@
+setup(
+    name='Number Generator',
+    version='0.1', packages=find_packages(exclude=['tests*']), license='MIT',
+    description='Number generator’, url='https://github.com/LunarNode/Lab4test.git', author='CanJiang_ValVeeramani',
+    author_email='violincellojc@gmail.com, VeeramaniVal@gmail.com'
+)
