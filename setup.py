aa2495dc8c42af61f25c2b245023dc1e7368214b
LunarNode <veeramanival@gmail.com>
2021-12-14 17:36:01 -0800

    minor change

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+from setuptools import setup, find_packages
+
 setup(
     name='Number Generator',
-    version='0.1', packages=find_packages(exclude=['tests*']), license='MIT',
+    version='0.1', packages=find_packages(exclude=['tests']), license='MIT',
     description='Number generator', url='https://github.com/LunarNode/Lab4test.git', author='CanJiang_ValVeeramani',
     author_email='violincellojc@gmail.com, VeeramaniVal@gmail.com'
 )
