bf180ca48185bae5b890aba534f9ae67554bb525
LunarNode <veeramanival@gmail.com>
2021-12-16 12:14:42 -0800

    Finished the video

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Number Generator',
+    name='GenerateRandomNumber',
     version='0.1', packages=find_packages(exclude=['tests']), license='MIT',
-    description='Number generator', url='https://github.com/LunarNode/Lab4test.git', author='CanJiang_ValVeeramani',
-    author_email='violincellojc@gmail.com, VeeramaniVal@gmail.com'
+    description='GenerateRandomNumber', url='https://github.com/LunarNode/Lab4test.git', author='CanJiang_ValVeeramani',
+    author_email='jiang607@purdue.edu'
 )
